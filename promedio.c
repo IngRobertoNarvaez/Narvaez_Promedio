bb30215acf6b76de220fe0b0bd6b2ef4bc588ff6
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-12-06 16:18:11 -0500

    Mostrar los 23 estudiantes sus promedios

--- promedio.c
+++ promedio.c
@@ -3,8 +3,36 @@
 #include <time.h>
 int main(int argc, char const *argv[])
 {
-    int primeroprogreso;
-    int segundoprogreso;
-    int tercerprogreso;
+    /*Se declara los progresos y cantidad*/
+    int primeroprogreso[23];
+    int segundoprogreso[23];
+    int tercerprogreso[23];
+    for (int i = 0; i < 23; i++)
+    {
+        primeroprogreso[i] = rand() % 10 + 1;
+    }
+    for (int i = 0; i < 23; i++)
+    {
+        segundoprogreso[i] = rand() % 10 + 1;
+    }
+    for (int i = 0; i < 23; i++)
+    {
+        tercerprogreso[i] = rand() % 10 + 1;
+    }
+    for (int i = 0; i < 23; i++)
+    {
+        printf("%d ", primeroprogreso[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 23; i++)
+    {
+        printf("%d ", segundoprogreso[i]);
+    }
+    printf("\n");
+    for (int i = 0; i < 23; i++)
+    {
+        printf("%d ", tercerprogreso[i]);
+    }
+
     return 0;
 }
