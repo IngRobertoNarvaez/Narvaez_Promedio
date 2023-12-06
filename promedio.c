4a11f59a58bde947733587b4c078fcc6cac3b18f
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-12-06 16:07:43 -0500

    Inicio el codigo

---
+++ promedio.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main(int argc, char const *argv[])
+{
+    int primeroprogreso;
+    int segundoprogreso;
+    int tercerprogreso;
+    return 0;
+}
